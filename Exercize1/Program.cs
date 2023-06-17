a10e2ad1d89c6134c0cbbc244c9d9b490f1009a6
Dmitriy <x5retail20@gmail.com>
2023-06-17 14:47:55 +0300

    задание 5

---
+++ Exercize1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество чисел: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        int positiveCount = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше 0: {positiveCount}");
+    }
+}
---
+++ Exercize2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значения для первой прямой (y = k1 * x + b1):");
+        double k1 = ReadDouble("k1");
+        double b1 = ReadDouble("b1");
+
+        Console.WriteLine("Введите значения для второй прямой (y = k2 * x + b2):");
+        double k2 = ReadDouble("k2");
+        double b2 = ReadDouble("b2");
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+    }
+
+    static double ReadDouble(string name)
+    {
+        Console.Write($"Введите значение {name}: ");
+        return Convert.ToDouble(Console.ReadLine());
+    }
+}
